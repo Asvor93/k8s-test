a97023260fe022ea6e00a01127f90c5c8fd638b8
Asvør Rasmussen <asvor_rasmussen@hotmail.com>
2021-06-17 15:12:12 +0200

    first commit

---
+++ src/views/Fact.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+import { defineComponent, computed, ComputedRef } from 'vue'
+import { useRoute} from 'vue-router'
+import FactCard from '@/components/FactCard.vue'
+
+export default defineComponent({
+    components: { FactCard },
+    setup() {
+        const router = useRoute()
+    }
+})
+</script>
+
+<template>
+    <FactCard :fact-id="parseInt(factId)" />
+</template>
